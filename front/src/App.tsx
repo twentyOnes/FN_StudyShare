33b4d205c590370aff072b6b115f62ce6c9c4cd3
이현주 <hyunmail521@gmail.com>
2021-04-14 01:27:20 +0900

    SET: project dev  setting

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@/styles/App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">안녕하세요</header>
+    </div>
+  );
+};
+
+export default App;
