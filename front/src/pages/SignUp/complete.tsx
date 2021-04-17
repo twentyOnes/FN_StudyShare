5f7eba15b765bd6dc80370c278721f74ab44783a
이현주 <hyunmail521@gmail.com>
2021-04-17 15:36:38 +0900

    Add: SignUp Complete Page

--- front/src/pages/SignUp/complete.tsx
+++ front/src/pages/SignUp/complete.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-// import "@/styles/App.scss";
+import { Button, CompleteWrap } from "./styled";
 
 const SignUpComplete: React.FC = () => {
-  return <div>회원가입완료</div>;
+  return (
+    <CompleteWrap>
+      <div>
+        <p>
+          회원가입이 완료되었습니다. <br />
+          <b>Study Share</b>와 함께
+          <br />
+          계획적으로 스터디를 관리하세요!
+        </p>
+
+        <Button type="button">Study Share 시작하기</Button>
+      </div>
+    </CompleteWrap>
+  );
 };
 
 export default SignUpComplete;
